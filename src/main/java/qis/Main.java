b0fe21f94f3ab71eac29cd11b0abc0b28d11ec75
andrei <kk>
2019-02-13 12:25:55 +0100

    user and pass request in cli

--- src/main/java/qis/Main.java
+++ src/main/java/qis/Main.java
@@ -1,14 +1,24 @@
 package qis;
+
+import java.io.Console;
+
 public class Main {
     public static void main(String[] args){
         // Das UI für das Programm initialisieren
         QISUi ui = new QISUi();
         // Simple UI- Steuerung
-        if(args.length >= 3 && args.length <=3){
+        if(args.length >= 1 && args.length <=1){
             if(Integer.parseInt(args[0]) == 1){
-                ui.zeigeAlleNotenAn(args[1],args[2]);
+                System.out.println("Ausgabe der Noten aus dem QIS-Prüfungssystem");
+                Console console = System.console();
+                String username = console.readLine("Benutzername: ");
+                String password = new String(console.readPassword("Passwort: "));
+                ui.zeigeAlleNotenAn(username,password);
             }else if(Integer.parseInt(args[0]) == 2){
-                ui.zeigeDurchschnittAn(args[1],args[2]);
+                Console console = System.console();
+                String username = console.readLine("Benutzername: ");
+                String password = new String(console.readPassword("Passwort: "));
+                ui.zeigeDurchschnittAn(username,password);
             }
             }else{
                 ui.zeigeHilfeAn();
